7336348fa78bd702a97c3ea3612d9079a1cefc96
Lillian <lillian.merrill27@student.tamdistrict.org>
2025-02-28 15:15:31 +0000

    added images

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -13,23 +13,31 @@ export class Game extends Scene
 
     playButton: Phaser.GameObjects.Image;
 
-
-
-
     constructor ()
     {
         super('Game');
+    }
+
+    preload ()
+    {
+        this.load.image('destroyer', 'assets/destroyerShip.png');
+        this.load.image('submarine', 'assets/submarine.png');
+        this.load.image('cruiser', 'assets/cruiser.png');
+        this.load.image('battleship', 'assets/battleship.png');
+        this.load.image('carrier', 'assets/battleship.png');
     }
 
     create ()
     {
         this.cursor = this.input.keyboard!.createCursorKeys(); 
 
-        this.add.image(60,60,'background')
+        this.add.image(60,60,'background');
+        this.add.image(30,30,'destroyer');
 
         this.grid = this.physics.add.staticGroup();
 
         this.playButton = this.add.image(1,2, 'button', 1).setInteractive();
+
 
     }
 }
